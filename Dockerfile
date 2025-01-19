e8d64a4c7da5f8de891a34563c12b1849e42ca6a
David Symonds <dsymonds@gmail.com>
2025-01-19 14:24:13 +1100

    Export local_time metrics in specified time zones.
    Simplify metric construction using the promauto package, and update the
    versions of Go and Alpine Linux used in the Docker image build.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # Build this with
 #   docker build -t dsymonds/solarmon .
 
-FROM golang:1.22-alpine AS build
+FROM golang:1.23-alpine AS build
 
 WORKDIR /go/src/solarmon
 COPY go.mod go.sum ./
@@ -14,7 +14,10 @@ RUN go build -o solarmon -v
 
 # -----
 
-FROM alpine:3.18 AS runtime
+FROM alpine:3.21 AS runtime
+
+# Include tzdata for tracking local times.
+RUN apk add --no-cache tzdata
 
 COPY --from=build /go/src/solarmon/solarmon /
 ENTRYPOINT ["/solarmon"]
--- main.go
+++ main.go
@@ -5,76 +5,93 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
 	enphaseAddr = flag.String("enphase_addr", "", "IP address of Enphase Envoy")
 	port        = flag.Int("port", 0, "port to run on")
+
+	timezones = flag.String("timezones", "", "comma-separated list of timezones to export gauges for")
 )
 
-// enphaseCollector implements prometheus.Collector.
-type enphaseCollector struct {
+var (
+	up = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "up",
+		Help: "Whether the Enphase Envoy is responding",
+	})
+	genNow = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "power_production_watts",
+		Help: "Power being produced, in W",
+	})
+	genToday = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "energy_today_watthours",
+		Help: "Energy produced today, in Wh",
+	})
+	// In theory this should always increase, but we can't guarantee it,
+	// so leave as a gauge instead of a counter. It's only read as snapshots anyway.
+	genLifetime = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "energy_lifetime_watthours",
+		Help: "Energy produced in the system's lifetime, in Wh",
+	})
+	localTime = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "local_time",
+		Help: "Local time (hour and fractional minute)",
+	}, []string{"tz"})
+)
+
+// enphaseMonitor monitors an Enphase Envoy.
+type enphaseMonitor struct {
 	addr string
-
-	up, genNow, genToday, genLifetime prometheus.Gauge
+	tzs  map[string]*time.Location
 }
 
-func newEnphaseCollector(addr string) *enphaseCollector {
-	ec := &enphaseCollector{
-		addr: addr,
-		up: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "up",
-			Help: "Whether the Enphase Envoy is responding",
-		}),
-		genNow: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "power_production_watts",
-			Help: "Power being produced, in W",
-		}),
-		genToday: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "energy_today_watthours",
-			Help: "Energy produced today, in Wh",
-		}),
-		// In theory this should always increase, but we can't guarantee it,
-		// so leave as a gauge instead of a counter. It's only read as snapshots anyway.
-		genLifetime: prometheus.NewGauge(prometheus.GaugeOpts{
-			Name: "energy_lifetime_watthours",
-			Help: "Energy produced in the system's lifetime, in Wh",
-		}),
+func newEnphaseMonitor(addr string) (*enphaseMonitor, error) {
+	tzs := make(map[string]*time.Location)
+	if *timezones != "" {
+		for _, tz := range strings.Split(*timezones, ",") {
+			loc, err := time.LoadLocation(tz)
+			if err != nil {
+				return nil, fmt.Errorf("Loading location %q: %w", tz, err)
+			}
+			tzs[tz] = loc
+		}
 	}
-	return ec
+
+	return &enphaseMonitor{
+		addr: addr,
+		tzs:  tzs,
+	}, nil
 }
 
-func (ec *enphaseCollector) Describe(ch chan<- *prometheus.Desc) {
-	ec.up.Describe(ch)
-	ec.genNow.Describe(ch)
-	ec.genToday.Describe(ch)
-	ec.genLifetime.Describe(ch)
-}
-
-func (ec *enphaseCollector) Collect(ch chan<- prometheus.Metric) {
+func (em *enphaseMonitor) Refresh() {
 	// TODO: rate limit here
 	// TODO: set a HTTP timeout too. 5s should be plenty.
-	stats, err := fetchStats(ec.addr)
+	stats, err := fetchStats(em.addr)
 	if err != nil {
-		ec.up.Set(0)
-		ec.genNow.Set(0)
-		ec.genToday.Set(0)
-		ec.genLifetime.Set(0)
+		up.Set(0)
+		genNow.Set(0)
+		genToday.Set(0)
+		genLifetime.Set(0)
 	} else {
-		ec.up.Set(1)
-		ec.genNow.Set(float64(stats.WattsNow))
-		ec.genToday.Set(float64(stats.WattHoursToday))
-		ec.genLifetime.Set(float64(stats.WattHoursLifetime))
+		up.Set(1)
+		genNow.Set(float64(stats.WattsNow))
+		genToday.Set(float64(stats.WattHoursToday))
+		genLifetime.Set(float64(stats.WattHoursLifetime))
 		//log.Printf("Currently generating %.2f kW", float64(stats.WattsNow)/1e3)
 		//log.Printf("Generated %.1f kWh today", float64(stats.WattHoursToday)/1e3)
 	}
-	ch <- ec.up
-	ch <- ec.genNow
-	ch <- ec.genToday
-	ch <- ec.genLifetime
+
+	now := time.Now()
+	for tz, loc := range em.tzs {
+		h, m, _ := now.In(loc).Clock()
+		localTime.WithLabelValues(tz).Set(float64(h) + float64(m)/60)
+	}
 }
 
 func main() {
@@ -83,8 +100,16 @@ func main() {
 		log.Fatal("must provide -enphase_addr")
 	}
 
-	coll := newEnphaseCollector(*enphaseAddr)
-	prometheus.MustRegister(coll)
+	mon, err := newEnphaseMonitor(*enphaseAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	go func() {
+		mon.Refresh()
+		for range time.NewTicker(1 * time.Minute).C {
+			mon.Refresh()
+		}
+	}()
 
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
